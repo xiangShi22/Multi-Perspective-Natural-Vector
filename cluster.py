428248ec5fa704624e147b77c3216d5cb61bd84c
xiangShi <67421664+xiangShi22@users.noreply.github.com>
2025-09-13 00:25:16 +0800

    Update cluster.py

--- cluster.py
+++ cluster.py
@@ -41,7 +41,7 @@ ari = adjusted_rand_score(y, cluster_labels) if len(set(cluster_labels)) > 1 els
 nmi = normalized_mutual_info_score(y, cluster_labels) if len(set(cluster_labels)) > 1 else -1
 fmi = fowlkes_mallows_score(y, cluster_labels) if len(set(cluster_labels)) > 1 else -1
 
-print('\n聚类性能：')
+print('\nResults：')
 print(f"Silhouette Score: {ss:.4f}")
 print(f"Calinski-Harabasz Score: {chs:.4f}")
 print(f"Davies-Bouldin Score: {dbs:.4f}")
